b11e7b85e2f105345877044f28b342f120cfc002
Francisco Seita <franciscoseita2001@gmail.com>
2024-04-23 19:53:07 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, HTTPException
+from db.db import client
+from controller.usuarioCRUD import router as usuarios_router
+
+
+app = FastAPI()
+
+app.include_router(usuarios_router, tags=["usuarios"], prefix="/usuarios")
+# MongoDB connection URL
+@app.on_event("shutdown")
+def shutdown_db_client():
+    client.close()
+
+
+
